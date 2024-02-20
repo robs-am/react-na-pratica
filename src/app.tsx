10e2e95ae89f9762dcba88aac970b27fe18f834a
Roberta <roberta.amaro@ed3.com.br>
2024-02-20 11:16:53 -0300

    adiciona header

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,20 @@
 import { Plus } from 'lucide-react';
+import { Header } from './components/header';
+import { Tabs } from './components/tabs';
+import { Button } from './components/ui/button';
 
 export function App() {
   return (
     <div className="py-10 space-y-8">
-      <div>header tabs</div>
+      <Header />
+      <Tabs />
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <button className="inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-2 py-1">
+          <Button variant="primary">
             <Plus className="size-3" />
             Create new
-          </button>
+          </Button>
         </div>
         <div className="flex items-center justify-between"></div>
       </main>
